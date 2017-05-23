0c8e6ae9c5d1fd02279514d241538b19e076266f
Wojciech Olczak <byzl1360@gmail.com>
2017-05-23 21:05:42 +0200

    Modele początek

---
+++ bookreview/bookreview/Models/BaseModels/Author.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookreview.Models.BaseModels
+{
+    public class Author
+    {
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public DateTime BirthDate { get; private set; }
+
+        public string Bio { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime UpdatedAt { get; private set; }
+
+
+        public Author(string firstName, string lastName, DateTime birthDate, string bio)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Bio = bio;
+            CreatedAt = UpdatedAt = DateTime.Now;
+        }
+
+    }
+}
