65036eba3bc5d07e3f96b6a32644881842359f86
Wojciech Olczak <byzl1360@gmail.com>
2017-05-28 15:44:25 +0200

    Modele

---
+++ bookreview/bookreview/Models/BaseModels/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookreview.Models.BaseModels
+{
+
+    public class Book
+    {       
+        public string Name { get; private set; }
+
+        public Author Author { get; private set; }
+
+        public DateTime ReleaseDate { get; private set; }
+
+        public string Description { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime UpdatedAt { get; private set; }
+
+
+        public Book(string name, Author author, DateTime releaseDate, string description)
+        {
+            Name = name;
+            Author = author;
+            ReleaseDate = releaseDate;
+            Description = description;
+            CreatedAt = UpdatedAt = DateTime.Now;
+        }
+                
+    }
+}
---
+++ bookreview/bookreview/Models/BaseModels/Review.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookreview.Models.BaseModels
+{
+    public class Review
+    {
+        public ApplicationUser User { get; private set; }
+
+        public bool EntityType { get; private set; }
+
+        public Object Entity { get; private set; }
+
+        public string Text { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime UpdatedAt { get; private set; }
+
+
+        public Review(ApplicationUser user, bool entityType, Object entity, string text)
+        {
+            User = user;
+            EntityType = entityType;
+            Entity = entity;
+            Text = text;
+            CreatedAt = UpdatedAt = DateTime.Now;
+        }
+
+    }
+}
