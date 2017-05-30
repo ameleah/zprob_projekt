8a20da36bd7b0e732cb497b1eae452efd993958c
Wojciech Olczak <byzl1360@gmail.com>
2017-05-30 20:40:52 +0200

    NUnit tests

---
+++ bookreview/bookreview.Tests/Models/CategoryTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bookreview.Models.BaseModels;
+using bookreview.Models;
+
+namespace bookreview.Tests.Models
+{
+    [TestFixture]
+    public class CategoryTests
+    {
+        [Test]
+        public void AddCategoryWithoutName()
+        {
+            Assert.Throws<Exception>(new TestDelegate(() => new Category(null)));            
+        }
+    }
+}
